a367d211bfeffccc4a4a5e8fce9e7be8d44b90b0
Slyke <steven.lawler777@gmail.com>
2020-01-06 14:43:34 -0800

    Fixed bug with directories

--- install.sh
+++ install.sh
@@ -3,13 +3,17 @@
 touch ~/.bashrc
 mkdir -p ~/.bashstyle
 
-cp ./bashrc_inject ~/.bashstyle/bashrc_inject
-cp ./colors.conf ~/.bashstyle/colors.conf
-cp ./install.sh ~/.bashstyle/reinstall.sh
-
 if grep -qF ". ~/.bashstyle/bashrc_inject" ~/.bashrc;then
+  cp ./bashrc_inject ~/.bashstyle/bashrc_inject
+  cp ./colors.conf ~/.bashstyle/colors.conf
+  cp ./install.sh ~/.bashstyle/reinstall.sh
+
   echo "BashStyle is already installed in '~/.bashrc'. Replaced source files in '~/.bashstyle/'"
 else
+  cp ./bashrc_inject ~/.bashstyle/bashrc_inject
+  cp ./colors.conf ~/.bashstyle/colors.conf
+  cp ./install.sh ~/.bashstyle/reinstall.sh
+
   echo 'if [ -f ~/.bashstyle/bashrc_inject ]; then' >> ~/.bashrc
   echo '    . ~/.bashstyle/bashrc_inject' >> ~/.bashrc
   echo 'fi' >> ~/.bashrc
