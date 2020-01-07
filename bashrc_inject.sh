d2fa8eced68febd290ec4000fb1d6e99f2f22b14
Slyke <steven.lawler777@gmail.com>
2020-01-07 12:38:24 -0800

    Removed DOS EoLs

--- bashrc_inject.sh
+++ bashrc_inject.sh
@@ -6,7 +6,7 @@ export CLICOLOR=1
 
   function color_my_prompt {
     local date_and_time="\[${RED}\][`date +%FT%T%z`]"
-    local user_and_host="\[${wHITE}\][\u@$(hostname)]"
+    local user_and_host="${wHITE}\[\][${gREEN}\u@$(hostname)${wHITE}]"
     local current_location="\[${wHITE}\]\W"
     local git_branch_color="\[${pURPLE}\]"
     local git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`'
