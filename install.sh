806747bd02d344f2d8a3d4913304cc4b0988744f
Slyke <steven.lawler777@gmail.com>
2020-01-07 12:16:04 -0800

    Removed colors causing errors

--- install.sh
+++ install.sh
@@ -4,18 +4,18 @@ touch ~/.bashrc
 mkdir -p ~/.bashstyle
 
 if grep -qF ". ~/.bashstyle/bashrc_inject" ~/.bashrc;then
-  cp ./bashrc_inject ~/.bashstyle/bashrc_inject
+  cp ./bashrc_inject.sh ~/.bashstyle/bashrc_inject.sh
   cp ./colors.conf ~/.bashstyle/colors.conf
   cp ./install.sh ~/.bashstyle/reinstall.sh
 
   echo "BashStyle is already installed in '~/.bashrc'. Replaced source files in '~/.bashstyle/'"
 else
-  cp ./bashrc_inject ~/.bashstyle/bashrc_inject
+  cp ./bashrc_inject.sh ~/.bashstyle/bashrc_inject.sh
   cp ./colors.conf ~/.bashstyle/colors.conf
   cp ./install.sh ~/.bashstyle/reinstall.sh
 
-  echo 'if [ -f ~/.bashstyle/bashrc_inject ]; then' >> ~/.bashrc
-  echo '    . ~/.bashstyle/bashrc_inject' >> ~/.bashrc
+  echo 'if [ -f ~/.bashstyle/bashrc_inject.sh ]; then' >> ~/.bashrc
+  echo '    . ~/.bashstyle/bashrc_inject.sh' >> ~/.bashrc
   echo 'fi' >> ~/.bashrc
   echo "BashStyle installed."
 fi
