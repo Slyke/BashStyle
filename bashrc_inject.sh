b11b8ab4e3e415b7423a81b25ded091768c686c7
Slyke <steven.lawler777@gmail.com>
2020-07-27 00:15:00 -0700

    Fixed typo in datetime

--- bashrc_inject.sh
+++ bashrc_inject.sh
@@ -5,7 +5,7 @@ export GIT_PS1_SHOWUNTRACKEDFILES=true
 export CLICOLOR=1
 
   function color_my_prompt {
-    local date_and_time="\[${RED}\][\$(date +%FT%T%z)\]"
+    local date_and_time="\[${RED}\][\$(date +%FT%T%z)]"
     local user_and_host="${wHITE}\[\][${gREEN}\u@$(hostname)${wHITE}]"
     local current_location="\[${wHITE}\]\W"
     local git_branch_color="\[${pURPLE}\]"
